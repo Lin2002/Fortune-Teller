881251600333055be59780ccfc91b3b329c5876c
Lin2002 <33724487+Lin2002@users.noreply.github.com>
2019-01-18 09:26:19 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$("button").click(function(){
+    var q1 = $("#in1").val(); var q2 = $("#in2").val(); var q3 = $("#in3").val();
+
+   
+    $("p").text(q1+" will play "+q3*1616.43+" hours of "+q2+" by the time of 2050!");
+
+    });
